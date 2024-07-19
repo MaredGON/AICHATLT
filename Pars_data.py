01d9aa437f852a17744c8b7ce542f0cbcd4b3439
a.zhelvakov <a.zhelvakov@otus.ru>
2024-07-20 02:18:45 +0300

    black fix

--- Pars_data.py
+++ Pars_data.py
@@ -1,5 +1,5 @@
-
 from playwright.sync_api import sync_playwright
+
 
 def scrape_website(url):
     with sync_playwright() as p:
@@ -8,7 +8,7 @@ def scrape_website(url):
         page.goto(url)
 
         # Дождаться, пока страница загрузится
-        page.wait_for_load_state('networkidle')
+        page.wait_for_load_state("networkidle")
 
         # Получить весь текст с страницы
         content = page.content()
@@ -16,20 +16,22 @@ def scrape_website(url):
         browser.close()
         return content
 
+
 def save_to_file(content, filename):
-    with open(filename, 'w', encoding='utf-8') as file:
+    with open(filename, "w", encoding="utf-8") as file:
         file.write(content)
 
+
 urls = {
-    'latoken_info': 'https://coda.io/@latoken/latoken-talent/latoken-161',
-    'hackathon_info': 'https://deliver.latoken.com/hackathon',
-    'rag_culture_deck': 'https://coda.io/@latoken/latoken-talent/culture-139'
+    "latoken_info": "https://coda.io/@latoken/latoken-talent/latoken-161",
+    "hackathon_info": "https://deliver.latoken.com/hackathon",
+    "rag_culture_deck": "https://coda.io/@latoken/latoken-talent/culture-139",
 }
 
 for name, url in urls.items():
     content = scrape_website(url)
     if content:
-        save_to_file(content, f'{name}.txt')
+        save_to_file(content, f"{name}.txt")
         print(f"Информация {name} сохранена")
     else:
         print(f"Не удалось получить данные с {url}")
--- main.py
+++ main.py
@@ -12,9 +12,9 @@ from openai import AsyncOpenAI
 
 TOKEN = "6440161421:AAEiJX0-Q75h0mv4tBT5dJ0gfD_jXraRUQY"
 GPT_TOKEN = "sk-proj-8f2n9OhSdDhYcKoWEkiBT3BlbkFJTye8GS9wRoQC2pbDkA9I"
-latoken_info = 'https://coda.io/@latoken/latoken-talent/latoken-161'
-latoken_hackathon = 'https://deliver.latoken.com/hackathon'
-latoken_culture = 'https://coda.io/@latoken/latoken-talent/culture-139'
+latoken_info = "https://coda.io/@latoken/latoken-talent/latoken-161"
+latoken_hackathon = "https://deliver.latoken.com/hackathon"
+latoken_culture = "https://coda.io/@latoken/latoken-talent/culture-139"
 START_PROMT = f"Смотри, теперь ты AI ассистент в компании LATOKEN. Проанализируй эти три ссылке: {latoken_info} {latoken_hackathon} {latoken_culture}. Используй эту инфромацию при генерации ответа, а так же для каждого ответа в конце указывай - Более подробную информацию вы можете получить по ссылке *тут нужная ссылка*."
 client = AsyncOpenAI(
     api_key=GPT_TOKEN,
@@ -23,10 +23,10 @@ dp = Dispatcher()
 
 
 async def request_for_response(promt):
-    response = ''
+    response = ""
     stream = await client.chat.completions.create(
         model="gpt-4o",
-        messages= [{'role': 'user', 'content': promt}],
+        messages=[{"role": "user", "content": promt}],
         stream=True,
     )
     async for chunk in stream:
@@ -34,17 +34,19 @@ async def request_for_response(promt):
 
     return response
 
+
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
 
-    await message.answer(f"Привет, {message.from_user.full_name}! Я уникальный AI ассистент LATOKEN. Я нужен для подбора участников на хакатон!\n"
-                         f"Задайте вопрос который хотите.")
+    await message.answer(
+        f"Привет, {message.from_user.full_name}! Я уникальный AI ассистент LATOKEN. Я нужен для подбора участников на хакатон!\n"
+        f"Задайте вопрос который хотите."
+    )
 
 
 @dp.message()
 async def echo_handler(message: Message) -> None:
     try:
-        promt =
         text = await request_for_response(message.text)
         await message.answer(text)
     except TypeError:

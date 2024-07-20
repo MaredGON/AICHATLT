988c7f5f064be52154b0dc0cb399b3edbaa5877c
Matvey <sgrinkel@gmail.com>
2024-07-20 04:31:53 +0300

    d

--- main2.py
+++ main2.py
@@ -17,18 +17,26 @@ client = OpenAI(
     api_key=GPT_TOKEN,
 )
 
-files_to_upload = ["datalatoken.txt", "datahackathon.txt", "dataculture.txt"]
+files_to_upload = ["./datainfo/datalatoken.txt", "./datainfo/datahackathon.txt", "./datainfo/dataculture.txt"]
+file1 = client.files.create(
+  file=open(files_to_upload[0], "rb"),
+  purpose='assistants'
+)
+file2 = client.files.create(
+  file=open(files_to_upload[1], "rb"),
+  purpose='assistants'
+)
+file3 = client.files.create(
+  file=open(files_to_upload[2], "rb"),
+  purpose='assistants'
+)
 
-uploaded_files = []
-for file_name in files_to_upload:
-    with open(file_name, "rb") as file:
-        uploaded_file = client.files.create(file=file, purpose="assistants")
-        uploaded_files.append(uploaded_file)
+
 
 assistant = client.beta.assistants.create(
     name="Ты AI ассистент компании LATOKEN",
     description="Ты AI ассистент, который берет информацию о компании из datalatoken.txt, информацию о хакатоне из datahackathon.txt, информацию о культуре из dataculture.txt. Ты нужен кандидатам чтобы отвечать на вопросы: о компании, культуре, хакатоне. ",
     model="gpt-4o",
-    tools=[{"type": "retrieval"}],
-    tool_resources={"code_interpreter": {"file_ids": [uploaded_files[0]]}},
+    tools=[{"type": "file_search"}],
+    tool_resources={"code_interpreter": {"file_ids": [file1.id, file2.id, file3.id]}},
 )

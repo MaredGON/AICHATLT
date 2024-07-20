4c05c0a58236e85e2f838872c067ed49760e6d3a
a.zhelvakov <a.zhelvakov@otus.ru>
2024-07-20 03:20:47 +0300

    upd

--- config.py
+++ config.py
@@ -24,3 +24,49 @@ START_PROMT = (
     f"{latoken_info} {latoken_hackathon} {latoken_culture}. Используй эту инфромацию при генерации ответа, "
     "а так же для каждого ответа в конце указывай - Более подробную информацию вы можете получить по ссылке *тут нужная ссылка*."
 )
+
+
+
+class Promts:
+    company_info = "./datainfo/datalatoken.txt"
+    hackathon_info = "./datainfo/datahackathon.txt"
+    culture_info = "./datainfo/dataculture.txt"
+
+    @staticmethod
+    def get_company_info():
+        with open(Promts.company_info, "r", encoding="utf-8") as file:
+            return file.read()
+
+    @staticmethod
+    def get_hackathon_info():
+        with open(Promts.hackathon_info, "r", encoding="utf-8") as file:
+            return file.read()
+
+    @staticmethod
+    def get_culture_info():
+        with open(Promts.culture_info, "r", encoding="utf-8") as file:
+            return file.read()
+
+    @staticmethod
+    def default_promt():
+        return f"""
+        Ты - полезный ассистент, помогающий пользователям с вопросами по компании LATOKEN биржа.
+        Вот информация о компании LATOKEN биржа:
+        Основная ифнормация: {Promts.get_company_info()}
+        Информация о хакатоне: {Promts.get_hackathon_info()}
+        Информация о культуре: {Promts.get_culture_info()}
+        
+        При ответе пользователю ты не должен выдавать весь текст файла.
+        Ты должен отвечать только на вопросы. Твои ответы должны быть приведены в человекоподобный вид.
+        Так, чтобы живому человеку было удобно их читать.
+        
+        Не используй выделение форматиррование текста. Например: **Какой-то текст**. Так делать не надо.
+        """
+
+    @staticmethod
+    def user_message_promt(user_message):
+        return f"""
+        {Promts.default_promt()}
+
+        Вопрос пользователя: {user_message}
+        """
--- gpt.py
+++ gpt.py
@@ -1,17 +1,21 @@
-from config import TOKEN, client, START_PROMT
+from config import TOKEN, client, START_PROMT, Promts
 
 
-async def request_for_response(promt: str) -> str:
+async def request_for_response(user_message: str) -> str:
     """
     Отправляет запрос к API OpenAI и возвращает ответ.
 
     :param promt: Текст запроса.
     :return: Ответ от API OpenAI.
     """
+
     response = ""
     stream = await client.chat.completions.create(
         model="gpt-4o",
-        messages=[{"role": "user", "content": promt}],
+        messages=[
+            {"role": "system", "content": Promts.user_message_promt(user_message)},
+            {"role": "user", "content": user_message},
+        ],
         stream=True,
     )
     async for chunk in stream:
